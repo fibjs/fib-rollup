eec0e940803628a10aab9696f324df67989cfe3d
Richardo2016 <richardo2016@gmail.com>
2018-09-07 16:48:39 +0800

    feat: add exported member 'recommendedVBoxModules', 'recommendedVBoxModuleFallback', 'getCustomizedVBox' to support customize vbox.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-export const vbox = require('./vbox').default
+export {
+    default as vbox,
+    recommendedVBoxModules,
+    recommendedVBoxModuleFallback,
+    getCustomizedVBox
+} from './vbox'
+
+import { vbox } from './'
+
 export const fibjsResolve = require('./resolve')
 
 export default vbox.require('rollup', __dirname);
--- src/vbox/index.ts
+++ src/vbox/index.ts
@@ -1,29 +1,39 @@
-const vm = require('vm');
-const util = require('util');
-const builtinModules = util.buildInfo().modules
+import vm = require('vm');
+import util = require('util');
 
 import PatchedModule from '../patched-module'
 
-export const vbox = new vm.SandBox(
-    {
-        path: require('path'),
-        fs: require('fs'),
-        events: require('events'),
-        util: {
-            deprecate: require('util-deprecate/browser'),
-            ...require('util'),
-        },
-        crypto: require('crypto'),
-        buffer: require('buffer'),
-        // constants: {},
-        module: PatchedModule,
-        'builtin-modules': builtinModules
+export const builtinModules = util.buildInfo().modules
+export const recommendedVBoxModules = {
+    fs: require('fs'),
+    path: require('path'),
+    events: require('events'),
+    util: {
+        deprecate: require('util-deprecate/browser'),
+        ...require('util'),
     },
-    (name) => {
-        if (builtinModules.includes(name)) {
-            return require(name)
-        }
+    crypto: require('crypto'),
+    buffer: require('buffer'),
+    module: PatchedModule,
+    'builtin-modules': builtinModules
+}
+
+export function recommendedVBoxModuleFallback (name) {
+    if (builtinModules.includes(name)) {
+        return require(name)
     }
-);
+}
+
+export function getCustomizedVBox (myModules: any, myFallback: Function = recommendedVBoxModuleFallback) {
+    return new vm.SandBox(
+        {
+            ...recommendedVBoxModules,
+            ...myModules
+        },
+        myFallback
+    )
+}
+
+export const vbox = getCustomizedVBox({}, recommendedVBoxModuleFallback.bind(this))
 
 export default vbox
